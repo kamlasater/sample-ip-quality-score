b6c30d35745945ac388e96791db095bb11cd86e8
Kam Lasater <ckl@seekayel.com>
2021-06-03 16:55:41 -0400

    Added await

--- api/index.js
+++ api/index.js
@@ -5,12 +5,9 @@ exports.all = (req,res) => {
   const email = req.body['email']
   const apiKey = req.body['apiKey']
 
-  axios.get(`https://ipqualityscore.com/api/json/email/${apiKey}/${email}`)
-  .then((response) => {
-    console.log(response);
-    res.json(response.body)
-    res.status = response.status
-  }, (error) => {
-    console.log(error);
-  });
+  var response = await axios.get(`https://ipqualityscore.com/api/json/email/${apiKey}/${email}`)
+
+  console.log(response);
+  res.json(response.body)
+  res.status = response.status
 }
