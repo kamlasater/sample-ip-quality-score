a1b3b4b9732db69ffaa83f81d36c296a770900e6
Kam Lasater <ckl@seekayel.com>
2021-06-03 20:18:57 -0400

    not sure what is happening

--- api/index.js
+++ api/index.js
@@ -3,11 +3,13 @@ const axios = require('axios')
 module.exports.all = (req,res) => {
 
   console.log(req.body)
+  console.log(Object.keys(req.body))
 
   var e = req.body['email']
-  var a = req.body['apiKey']
+  // var a = req.body['apiKey']
+  var key = req.body.apiKey
 
-  (async (em,ak) => {
+  var r = (async (em,ak) => {
     console.log(em,ak)
     var response = await axios.get(`https://ipqualityscore.com/api/json/email/${ak}/${em}`)
     console.log(response);
@@ -15,8 +17,9 @@ module.exports.all = (req,res) => {
     // res.json(req.body)
     // res.json(response.body)
     // res.status = response.status
+    return "this is the result"
   })(e,a)
 
   console.trace()
-  console.log('test')
+  console.log('test',r)
 }
