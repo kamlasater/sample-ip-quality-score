e2158eaa796233dd719c05732b3095821beda5bd
Kam <seekayel@users.noreply.github.com>
2021-06-07 07:19:40 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,12 +4,11 @@ module.exports.all = async (req,res) => {
 
   console.log(req.body)
 
-  var body = JSON.parse(req.body)
+  var body = (req.body)? JSON.parse(req.body) : {}
 
   console.log(Object.keys(body))
 
-  var email = body.email
-  // var a = req.body['apiKey']
+  var email = req.params.email || body.email || 'email.missing@example.com'
   var key = process.env.cyclic_app_env_API_KEY || body.apiKey
 
   var response = await axios.get(`https://ipqualityscore.com/api/json/email/${key}/${email}`)
