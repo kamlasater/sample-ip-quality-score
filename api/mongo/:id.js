f85e82aa121d2cfadb350fdc12bd02428bb4f748
Kam Lasater <ckl@seekayel.com>
2021-08-17 07:31:31 -0400

    Pull by _id

--- api/mongo/:id.js
+++ api/mongo/:id.js
@@ -8,14 +8,19 @@ const uri = process.env.cyclic_app_env_MONGODB_URI;
 
 
 module.exports.get = async (req,res) => {
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+  await client.connect();
+
+  const result = await client.db("sample_airbnb").collection("listingsAndReviews").findOne({ _id: req.params.id });
+  if (result) {
+      console.log(`Found a listing in the collection with the name '${nameOfListing}':`);
+      console.log(result);
+  } else {
+      console.log(`No listings found with the name '${nameOfListing}'`);
+  }
 
   res.set('Content-Type','application/json')
   res.body = {env: process.env, req: req}
-  // const result = await client.db("sample_airbnb").collection("listingsAndReviews").findOne({ name: nameOfListing });
-  // if (result) {
-  //     console.log(`Found a listing in the collection with the name '${nameOfListing}':`);
-  //     console.log(result);
-  // } else {
-  //     console.log(`No listings found with the name '${nameOfListing}'`);
-  // }
+
 }
